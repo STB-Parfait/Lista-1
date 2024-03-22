fb35096c8f32c9ce1733578a8c45cee48a88d24d
jeser <jeserchivekalt@gmail.com>
2024-03-22 18:21:02 -0300

    Lista de execução

---
+++ Run.java
@@ -0,0 +1,24 @@
+public class Run {
+    public static void main(String[] args){
+    Ex1.main(args);
+    //Ex2.main(args);
+    //Ex3.main(args);
+    //Ex4.main(args);
+    //Ex5.main(args);
+    //Ex6.main(args);
+    //Ex7.main(args);
+    //Ex8.main(args);
+    //Ex9.main(args);
+    //Ex10.main(args);
+    //Ex11.main(args);
+    //Ex12.main(args);
+    //Ex13.main(args);
+    //Ex14.main(args);
+    //Ex15.main(args);
+    //Ex16.main(args);
+    //Ex17.main(args);
+    //Ex18.main(args);
+    //Ex19.main(args);
+    //Ex20.main(args);
+    }
+}
