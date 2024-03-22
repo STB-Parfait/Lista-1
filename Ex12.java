cc6e0dad46252b761901fe979ea3f1b22661fc61
jeser <jeserchivekalt@gmail.com>
2024-03-22 18:12:51 -0300

    Exercício 12

---
+++ Ex12.java
@@ -0,0 +1,112 @@
+public class Ex12 {
+    public static void main(String[] args){
+
+        int m = Prompt.lerInteiro();
+
+        switch(m){
+
+            case 1:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Janeiro");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 2:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Fevereiro");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 3:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Março");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 4:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Abril");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 5:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Maio");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 6:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Junho");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 7:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Julho");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 8:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Agosto");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 9:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Setembro");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 10:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Outubro");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 11:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Novembro");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            case 12:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Dezembro");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+            break;
+
+            default:
+            Prompt.separador();
+            Prompt.linhaEmBranco();
+            Prompt.imprimir("Mês Inválido!");
+            Prompt.linhaEmBranco();
+            Prompt.separador();
+        }
+    }
+}
