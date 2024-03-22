e05dc61ce0b295ffc44ec1663d6309e4d6e64b96
jeser <jeserchivekalt@gmail.com>
2024-03-22 18:17:06 -0300

    Exercício 16

---
+++ Ex16.java
@@ -0,0 +1,12 @@
+
+
+public class Ex16 {
+    public static void main(String[] args) {
+    	double a,b,c,f;
+    	a=Prompt.lerDecimal();
+    	b=Prompt.lerDecimal();
+    	c=Prompt.lerDecimal();
+    	f=(a+b+c)/3;
+    	System.out.printf("%.2f", f);
+    }
+}
